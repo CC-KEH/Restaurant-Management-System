f0ccddfa1b11c90055f720e34388b9977452d471
CC-KEH <arbashhussain08@gmail.com>
2024-10-17 10:56:56 +0530

    routes, db setup

--- app-server/src/db.rs
+++ app-server/src/db.rs
@@ -7,4 +7,38 @@ pub fn get_db_connection() -> Connection {
 
 pub fn initialize_db() {
     println!("Initializing the database...")
+    let conn = Connection::open("restaurant.db").expect("Failed to open SQLite connection");
+    conn.execute("PRAGMA foreign_keys=ON;",[]).expect("Failed to enable foreign key support");
+
+    println!("Create Table table");
+    creata_table_table(conn).expect("Failed to create Table table.");
+
+    println!("Create Menu table");
+    creata_menu_table(conn).expect("Failed to create Menu table.");
+
+    println!("Create Order table");
+    creata_order_table(conn).expect("Failed to create Order table.");
+
+    println!("Create OrderItem table");
+    creata_order_item_table(conn).expect("Failed to create OrderItem table.");
+}
+
+ fn creata_table_table(conn: &Connection) -> rusqlite::Result<()>{
+    conn.execute("CREATE TABLE IF NOT EXISTS tables(id INTEGER PRIMARY KEY, code TEXT NOT NULL UNIQUE)", [])?;
+    Ok(())
+}
+
+ fn creata_menu_table(conn: &Connection) -> rusqlite::Result<()>{
+    conn.execute("CREATE TABLE IF NOT EXISTS menus(id INTEGER PRIMARY KEY, name TEXT NOT NULL)", [])?;
+    Ok(())
+}
+
+ fn creata_order_table(conn: &Connection) -> rusqlite::Result<()>{
+    conn.execute("CREATE TABLE IF NOT EXISTS orders(id INTEGER PRIMARY KEY, table_id INTEGER NOT NULL, FOREIGN KEY (table_id) REFERENCES tables(id), UNIQUE(table_id))", [])?;
+    Ok(())
+}
+
+ fn creata_order_item_table(conn: &Connection) -> rusqlite::Result<()>{
+    conn.execute("CREATE TABLE IF NOT EXISTS order_items(id INTEGER PRIMARY KEY, order_id INTEGER NOT NULL, menu_id INTEGER NOT NULL, cooking_time NOT NULL, quantity INTEGER NOT NULL default 1, FOREIGN KEY (order_id) REFERENCES orders(id), FOREIGN KEY (menu_id) REFERENCES menus(id))", [])?;
+    Ok(())
 }
--- app-server/src/routes.rs
+++ app-server/src/routes.rs
@@ -1,3 +1,80 @@
-pub fn restaurant_routes(){
+use crate::db::get_db_connection;
+use crate::handlers::{
+    create_menu_handler, create_order_handler, create_table_handler,
+    delete_item_from_order_handler, get_item_from_order_handler, list_all_order_handler,
+    list_menu_handler, list_order_items_from_table_handler, list_table_handler,
+};
+use rusqlite::Connection;
+use std::convert::Infallible;
+use warp::{Filter, Rejection, Reply};
+
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    if error.if_not_found() {
+        Ok(warp::reply::with_status(
+            warp::reply::json(&format!("Error:{:?}", err)),
+            warp::http::StatusCode::NOT_FOUND,
+        ))
+    }
+    else if let Some(_) = err.find::<warp::filters.body::BodyDeserializeError>(){
+        Ok(warp::reply::with_status(
+            warp::reply::json(&format!("Error failed to deserialize request body")),
+            warp::http::StatusCode::BAD_REQUEST,
+        ))
+    }
+    else{
+        Ok(warp::reply_with_status(
+            warp_reply_json(&format!("Error {:?}",err)),
+            warp::http::StatusCode::INTER_SERVER_ERROR,
+        ))
+    }
+}
+
+pub fn create_order_route() {
     todo!()
+}
+
+pub fn create_table_route() {
+    todo!()
+}
+
+pub fn create_menu_route() {
+    todo!()
+}
+
+pub fn list_tables_route() {
+    todo!()
+}
+
+pub fn list_menus_route() {
+    todo!()
+}
+
+pub fn list_all_orders_route() {
+    todo!()
+}
+
+pub fn delete_item_from_order_route() {
+    todo!()
+}
+
+pub fn list_order_items_from_table_route() {
+    todo!()
+}
+
+pub fn get_item_from_order_route() {
+    todo!()
+}
+
+pub fn restaurant_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    let routes = create_order_route()
+        .or(create_table_route())
+        .or(create_menu_route())
+        .or(list_tables_route())
+        .or(list_menus_route())
+        .or(list_all_orders_route())
+        .or(delete_item_from_order_route())
+        .or(list_order_items_from_table_route())
+        .or(get_item_from_order_route());
+
+    routes.recover(handle_rejection)
 }
